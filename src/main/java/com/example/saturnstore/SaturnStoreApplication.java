b36009a4aaa12559c9fe2e6b91003a52e116b4cc
IvyVyn <iliya1997@gmail.com>
2022-03-18 20:00:11 +0200

    Initial commit

---
+++ src/main/java/com/example/saturnstore/SaturnStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.saturnstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturnStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaturnStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/saturnstore/SaturnStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saturnstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaturnStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
